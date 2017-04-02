7d3cefb91bd0064c8350e99ba3b2e26760ff0c82
Kequc <mr.kequc@gmail.com>
2017-04-02 19:54:13 +0200

    Update db tests

--- test/meta/db-meta.spec.js
+++ test/meta/db-meta.spec.js
@@ -10,27 +10,25 @@ describe('DbMeta', function() {
     describe('database does not exist', function() {
         it('head', function(done) {
             DbMeta.head(Helpers.Model, (err, res) => {
-                expect(err).to.not.be.undefined;
-                expect(err.name).to.equal('no_db_file');
-                done();
+                Helpers.EXPECT_ERROR(err, 'no_db_file');
+                Helpers.EXPECT_DB(false, done);
             });
         });
         it('create', function(done) {
             DbMeta.create(Helpers.Model, (err) => {
-                expect(err).to.be.undefined;
+                Helpers.EXPECT_ERROR(err);
                 Helpers.EXPECT_DB(true, done);
             });
         });
         it('destroy', function(done) {
             DbMeta.destroy(Helpers.Model, (err) => {
-                expect(err).to.not.be.undefined;
-                expect(err.name).to.equal('no_db_file');
+                Helpers.EXPECT_ERROR(err, 'no_db_file');
                 Helpers.EXPECT_DB(false, done);
             });
         });
         it('reset', function (done) {
             DbMeta.reset(Helpers.Model, (err) => {
-                expect(err).to.be.undefined;
+                Helpers.EXPECT_ERROR(err);
                 Helpers.EXPECT_DB(true, done);
             });
         });
@@ -39,26 +37,25 @@ describe('DbMeta', function() {
         beforeEach(Helpers.CREATE_DB);
         it('head', function(done) {
             DbMeta.head(Helpers.Model, (err, res) => {
-                expect(err).to.be.undefined;
-                done();
+                Helpers.EXPECT_ERROR(err);
+                Helpers.EXPECT_DB(true, done);
             });
         });
         it('create', function(done) {
             DbMeta.create(Helpers.Model, (err) => {
-                expect(err).to.not.be.undefined;
-                expect(err.name).to.equal('db_already_exists');
+                Helpers.EXPECT_ERROR(err, 'db_already_exists');
                 Helpers.EXPECT_DB(true, done);
             });
         });
         it('destroy', function(done) {
             DbMeta.destroy(Helpers.Model, (err) => {
-                expect(err).to.be.undefined;
+                Helpers.EXPECT_ERROR(err);
                 Helpers.EXPECT_DB(false, done);
             });
         });
         it('reset', function (done) {
             DbMeta.reset(Helpers.Model, (err) => {
-                expect(err).to.be.undefined;
+                Helpers.EXPECT_ERROR(err);
                 Helpers.EXPECT_DB(true, done);
             });
         });
